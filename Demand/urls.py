cc4ea2ebccbd742a5362d732509f802571d6c741
Harsh Satish Singh <56669968+HarshSingh74@users.noreply.github.com>
2020-08-23 22:43:11 +0530

    Update urls.py

--- Demand/urls.py
+++ Demand/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('prediction',views.prediction,name='prediction'),
-    path('reference',views.reference,name='reference')
+    path('reference',views.reference,name='reference'),
+    path('about',views.about,name='about')
 ]
